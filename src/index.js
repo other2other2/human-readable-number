ac0e30c06724c8d074c5f92ff184d1af876434b0
Vladislav Lekontsev <other2other2@gmail.com>
2023-01-12 17:29:27 +0400

    feat: do toReadable function
    just test description for commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let result = '';
+    const one = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+        'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const ten = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+    const toTextUnderHundred = (number) => {
+        if (number < 20) {
+            return one[number];
+        }
+        if (number < 100) {
+            return `${ten[Math.floor(number / 10)]} ${one[number % 10]}`
+        } else return toTextUnderThousand(number);
+    }
+
+    const toTextUnderThousand = (number) => {
+        if (number < 1000) {
+            let hundredQuantity = Math.floor(number / 100);
+            return `${one[hundredQuantity]} hundred ${toTextUnderHundred(number % 100)}`;
+        } else return toTextUnderMillion(number);
+    }
+
+    const toTextUnderMillion = (number) => {
+        if (number < 1000000) {
+            let thousandQuantity = Math.floor(number / 1000);
+            return `${toTextUnderHundred(thousandQuantity)} thousand ${toTextUnderHundred(number % 1000)}`;
+        }
+    }
+
+    if (number == 0 || typeof number !== 'number') {
+        return 'zero';
+    } else if (number >= 1000000) {
+        return 'to long number';
+    }
+
+    return toTextUnderHundred(number).trim();
 }
